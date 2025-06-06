db317b246cb37aee2d4e7be3a795da47fcfc4d3d
Olafur Arason <olafura@olafura.com>
2025-06-06 19:56:09 +0200

    chore: get openai token through env

--- config/config.exs
+++ config/config.exs
@@ -3,7 +3,7 @@ import Config
 config :rag_erlang_vm, ecto_repos: [RagErlangVm.Repo]
 
 config :rag_erlang_vm, RagErlangVm.Repo, types: RagErlangVm.PostgrexTypes
-config :rag_erlang_vm, openai_key: "your openai API key"
+config :rag_erlang_vm, openai_key: System.fetch_env!("OPENAI_TOKEN")
 config :nx, default_backend: EXLA.Backend
 
 import_config "#{config_env()}.exs"
